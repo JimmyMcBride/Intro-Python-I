e9f66ecee4b5330d3cb1b1acf5fbdc6aa6b4cd62
JimmyMcBride <mcbride967@gmail.com>
2020-01-18 12:44:28 -0600

    did most

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -1,19 +1,17 @@
-"""
-Dictionaries are Python's implementation of associative arrays.
-There's not much different with Python's version compared to what
-you'll find in other languages (though you can also initialize and
-populate dictionaries using comprehensions just like you can with
-lists!).
-
-The docs can be found here:
-https://docs.python.org/3/tutorial/datastructures.html#dictionaries
-
-For this exercise, you have a list of dictionaries. Each dictionary
-has the following keys:
- - lat: a signed integer representing a latitude value
- - lon: a signed integer representing a longitude value
- - name: a name string for this location
-"""
+# Dictionaries are Python's implementation of associative arrays.
+# There's not much different with Python's version compared to what
+# you'll find in other languages (though you can also initialize and
+# populate dictionaries using comprehensions just like you can with
+# lists!).
+#
+# The docs can be found here:
+# https://docs.python.org/3/tutorial/datastructures.html#dictionaries
+# 
+# For this exercise, you have a list of dictionaries. Each dictionary
+# has the following keys:
+#  - lat: a signed integer representing a latitude value
+#  - lon: a signed integer representing a longitude value
+#  - name: a name string for this location
 
 waypoints = [
     {
@@ -34,11 +32,15 @@ waypoints = [
 ]
 
 # Add a new waypoint to the list
-# YOUR CODE HERE
+waypoints.append({"lat": 42, "lon": -420, "name": "the best place"})
+print(waypoints)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
-# YOUR CODE HERE
+waypoints[0]["lon"] = -130
+waypoints[0]["name"] = "not a real place"
+print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
-# YOUR CODE HERE
+for i in range(len(waypoints)):
+    print(waypoints[i])
